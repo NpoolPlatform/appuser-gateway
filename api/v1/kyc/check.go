0a5c31365aeefde0460d58ed5da3de6d546d12f7
Zhao KK <kikakkz@hotmail.com>
2022-08-27 05:42:37 +0000

    Fix typo

--- api/v1/kyc/check.go
+++ api/v1/kyc/check.go
@@ -19,27 +19,27 @@ import (
 func validateKycCreate(ctx context.Context, info *kyc.CreateKycRequest) error {
 	if _, err := uuid.Parse(info.GetAppID()); err != nil {
 		logger.Sugar().Errorw("validateKycCreate", "AppID", info.GetAppID())
-		return status.Error(codes.InvalidArgument, "AppID is infovalid")
+		return status.Error(codes.InvalidArgument, "AppID is invalid")
 	}
 	if _, err := uuid.Parse(info.GetUserID()); err != nil {
 		logger.Sugar().Errorw("validateKycCreate", "UserID", info.GetUserID())
-		return status.Error(codes.InvalidArgument, "UserID is infovalid")
+		return status.Error(codes.InvalidArgument, "UserID is invalid")
 	}
 	if info.IDNumber != nil && info.GetIDNumber() == "" {
 		logger.Sugar().Errorw("validateKycCreate", "IDNumber", info.GetIDNumber())
-		return status.Error(codes.InvalidArgument, "IDNumber is infovalid")
+		return status.Error(codes.InvalidArgument, "IDNumber is invalid")
 	}
 	if info.GetFrontImg() == "" {
 		logger.Sugar().Errorw("validateKycCreate", "FrontImg", info.GetFrontImg())
-		return status.Error(codes.InvalidArgument, "FrontImg is infovalid")
+		return status.Error(codes.InvalidArgument, "FrontImg is invalid")
 	}
 	if info.BackImg != nil && info.GetBackImg() == "" {
 		logger.Sugar().Errorw("validateKycCreate", "BackImg", info.GetBackImg())
-		return status.Error(codes.InvalidArgument, "BackImg is infovalid")
+		return status.Error(codes.InvalidArgument, "BackImg is invalid")
 	}
 	if info.GetSelfieImg() == "" {
 		logger.Sugar().Errorw("validateKycCreate", "SelfieImg", info.GetSelfieImg())
-		return status.Error(codes.InvalidArgument, "SelfieImg is infovalid")
+		return status.Error(codes.InvalidArgument, "SelfieImg is invalid")
 	}
 
 	switch info.GetDocumentType() {
@@ -48,7 +48,7 @@ func validateKycCreate(ctx context.Context, info *kyc.CreateKycRequest) error {
 	case kycmgrpb.KycDocumentType_Passport:
 	default:
 		logger.Sugar().Errorw("validateKycCreate", "DocumentType", info.GetDocumentType())
-		return status.Error(codes.InvalidArgument, "DocumentType is infovalid")
+		return status.Error(codes.InvalidArgument, "DocumentType is invalid")
 	}
 
 	switch info.GetEntityType() {
@@ -56,7 +56,7 @@ func validateKycCreate(ctx context.Context, info *kyc.CreateKycRequest) error {
 	case kycmgrpb.KycEntityType_Organization:
 	default:
 		logger.Sugar().Errorw("validateKycCreate", "EntityType", info.GetEntityType())
-		return status.Error(codes.InvalidArgument, "EntityType is infovalid")
+		return status.Error(codes.InvalidArgument, "EntityType is invalid")
 	}
 
 	exist, err := mgrcli.ExistKycConds(ctx, &kycmgrpb.Conds{
@@ -84,27 +84,27 @@ func validateKycCreate(ctx context.Context, info *kyc.CreateKycRequest) error {
 func validateKycUpdate(ctx context.Context, info *kyc.UpdateKycRequest) error {
 	if _, err := uuid.Parse(info.GetAppID()); err != nil {
 		logger.Sugar().Errorw("validateKycUpdate", "AppID", info.GetAppID())
-		return status.Error(codes.InvalidArgument, "AppID is infovalid")
+		return status.Error(codes.InvalidArgument, "AppID is invalid")
 	}
 	if _, err := uuid.Parse(info.GetUserID()); err != nil {
 		logger.Sugar().Errorw("validateKycUpdate", "UserID", info.GetUserID())
-		return status.Error(codes.InvalidArgument, "UserID is infovalid")
+		return status.Error(codes.InvalidArgument, "UserID is invalid")
 	}
 	if info.IDNumber != nil && info.GetIDNumber() == "" {
 		logger.Sugar().Errorw("validateKycUpdate", "IDNumber", info.GetIDNumber())
-		return status.Error(codes.InvalidArgument, "IDNumber is infovalid")
+		return status.Error(codes.InvalidArgument, "IDNumber is invalid")
 	}
 	if info.FrontImg != nil && info.GetFrontImg() == "" {
 		logger.Sugar().Errorw("validateKycUpdate", "FrontImg", info.GetFrontImg())
-		return status.Error(codes.InvalidArgument, "FrontImg is infovalid")
+		return status.Error(codes.InvalidArgument, "FrontImg is invalid")
 	}
 	if info.BackImg != nil && info.GetBackImg() == "" {
 		logger.Sugar().Errorw("validateKycUpdate", "BackImg", info.GetBackImg())
-		return status.Error(codes.InvalidArgument, "BackImg is infovalid")
+		return status.Error(codes.InvalidArgument, "BackImg is invalid")
 	}
 	if info.SelfieImg != nil && info.GetSelfieImg() == "" {
 		logger.Sugar().Errorw("validateKycUpdate", "SelfieImg", info.GetSelfieImg())
-		return status.Error(codes.InvalidArgument, "SelfieImg is infovalid")
+		return status.Error(codes.InvalidArgument, "SelfieImg is invalid")
 	}
 
 	if info.IDNumber != nil && info.GetIDNumber() != "" {
