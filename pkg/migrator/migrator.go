4f6df8aa8f8f7cc96624b07e7c0de5b73f26c0f3
Zhao KK <kikakkz@hotmail.com>
2023-01-11 07:37:33 +0000

    Migration for app

--- pkg/migrator/migrator.go
+++ pkg/migrator/migrator.go
@@ -3,8 +3,25 @@ package migrator
 
 import (
 	"context"
+
+	"github.com/NpoolPlatform/appuser-manager/pkg/db"
+	"github.com/NpoolPlatform/appuser-manager/pkg/db/ent"
+
+	appctrlmgrpb "github.com/NpoolPlatform/message/npool/appuser/mgr/v2/appcontrol"
 )
 
 func Migrate(ctx context.Context) error {
-	return nil
+	if err := db.Init(); err != nil {
+		return err
+	}
+
+	return db.WithClient(ctx, func(_ctx context.Context, cli *ent.Client) error {
+		_, err := cli.
+			AppControl.
+			Update().
+			SetMaxTypedCouponsPerOrder(1).
+			SetCreateInvitationCodeWhen(appctrlmgrpb.CreateInvitationCodeWhen_Registration.String()).
+			Save(_ctx)
+		return err
+	})
 }
