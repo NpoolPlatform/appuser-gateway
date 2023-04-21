0a671bec7ed1101a8ac6ab7800850c9fb5dc7744
Zhao KK <kikakkz@hotmail.com>
2023-04-21 03:33:48 +0000

    Refactor creat app

--- api/app/create.go
+++ api/app/create.go
@@ -3,73 +3,52 @@ package app
 import (
 	"context"
 
-	commontracer "github.com/NpoolPlatform/appuser-gateway/pkg/tracer"
-	tracer "github.com/NpoolPlatform/appuser-middleware/pkg/tracer/app"
-
-	constant "github.com/NpoolPlatform/appuser-gateway/pkg/message/const"
-
-	appmwcli "github.com/NpoolPlatform/appuser-middleware/pkg/client/app"
-	appmwpb "github.com/NpoolPlatform/message/npool/appuser/mw/v1/app"
-
-	"github.com/NpoolPlatform/message/npool/appuser/gw/v1/app"
-
+	app1 "github.com/NpoolPlatform/appuser-gateway/pkg/app"
 	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
-
-	"go.opentelemetry.io/otel"
-	scodes "go.opentelemetry.io/otel/codes"
+	npool "github.com/NpoolPlatform/message/npool/appuser/gw/v1/app"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-
-	"github.com/google/uuid"
 )
 
-func (s *Server) CreateApp(ctx context.Context, in *app.CreateAppRequest) (*app.CreateAppResponse, error) {
-	var err error
-
-	_, span := otel.Tracer(constant.ServiceName).Start(ctx, "CreateApp")
-	defer span.End()
-	defer func() {
-		if err != nil {
-			span.SetStatus(scodes.Error, err.Error())
-			span.RecordError(err)
-		}
-	}()
-	appID := uuid.NewString()
-
-	appInfo := &appmwpb.AppReq{
-		ID:                       &appID,
-		CreatedBy:                &in.CreatedBy,
-		Name:                     &in.Name,
-		Logo:                     &in.Logo,
-		Description:              &in.Description,
-		SignupMethods:            in.SignupMethods,
-		ExtSigninMethods:         in.ExtSigninMethods,
-		RecaptchaMethod:          &in.RecaptchaMethod,
-		KycEnable:                &in.KycEnable,
-		SigninVerifyEnable:       &in.SigninVerifyEnable,
-		InvitationCodeMust:       &in.InvitationCodeMust,
-		CreateInvitationCodeWhen: &in.CreateInvitationCodeWhen,
-		MaxTypedCouponsPerOrder:  &in.MaxTypedCouponsPerOrder,
-		Maintaining:              &in.Maintaining,
-		CommitButtonTargets:      in.CommitButtonTargets,
-	}
-	span = tracer.Trace(span, appInfo)
-
-	err = validate(ctx, appInfo)
+func (s *Server) CreateApp(ctx context.Context, in *npool.CreateAppRequest) (*npool.CreateAppResponse, error) {
+	handler, err := app1.NewHandler(
+		ctx,
+		app1.WithCreatedBy(&in.CreatedBy),
+		app1.WithName(&in.Name),
+		app1.WithLogo(&in.Logo),
+		app1.WithDescription(&in.Description),
+		app1.WithSignupMethods(in.GetSignupMethods()),
+		app1.WithExtSigninMethods(in.GetExtSigninMethods()),
+		app1.WithRecaptchaMethod(&in.RecaptchaMethod),
+		app1.WithKycEnable(&in.KycEnable),
+		app1.WithSigninVerifyEnable(&in.SigninVerifyEnable),
+		app1.WithInvitationCodeMust(&in.InvitationCodeMust),
+		app1.WithCreateInvitationCodeWhen(&in.CreateInvitationCodeWhen),
+		app1.WithMaxTypedCouponsPerOrder(&in.MaxTypedCouponsPerOrder),
+		app1.WithMaintaining(&in.Maintaining),
+		app1.WithCommitButtonTargets(in.GetCommitButtonTargets()),
+	)
 	if err != nil {
-		return nil, err
+		logger.Sugar().Errorw(
+			"CreateApp",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.CreateAppResponse{}, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	span = commontracer.TraceInvoker(span, "app", "middleware", "CreateApp")
-
-	info, err := appmwcli.CreateApp(ctx, appInfo)
+	info, err := handler.CreateApp(ctx)
 	if err != nil {
-		logger.Sugar().Errorw("CreateApp", "err", err)
-		return &app.CreateAppResponse{}, status.Error(codes.Internal, err.Error())
+		logger.Sugar().Errorw(
+			"CreateApp",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.CreateAppResponse{}, status.Error(codes.Internal, err.Error())
 	}
 
-	return &app.CreateAppResponse{
+	return &npool.CreateAppResponse{
 		Info: info,
 	}, nil
 }
