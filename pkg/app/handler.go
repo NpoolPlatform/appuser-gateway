0733364a8892cf9f74754fc26545396613f8a528
Zhao KK <kikakkz@hotmail.com>
2023-04-21 02:23:18 +0000

    Add app handler

--- pkg/app/handler.go
+++ pkg/app/handler.go
@@ -2,12 +2,29 @@ package app
 
 import (
 	"context"
+	"fmt"
+
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
 
 	"github.com/google/uuid"
 )
 
 type Handler struct {
-	ID *string
+	ID                       *string
+	CreatedBy                *string
+	Name                     *string
+	Logo                     *string
+	Description              *string
+	SignupMethods            []basetypes.SignMethod
+	ExtSigninMethods         []basetypes.SignMethod
+	RecaptchaMethod          *basetypes.RecaptchaMethod
+	KycEnable                *bool
+	SigninVerifyEnable       *bool
+	InvitationCodeMust       *bool
+	CreateInvitationCodeWhen *basetypes.CreateInvitationCodeWhen
+	MaxTypedCouponsPerOrder  *uint32
+	Maintaining              *bool
+	CommitButtonTargets      []string
 }
 
 func NewHandler(ctx context.Context, options ...func(context.Context, *Handler) error) (*Handler, error) {
@@ -20,12 +37,169 @@ func NewHandler(ctx context.Context, options ...func(context.Context, *Handler) 
 	return handler, nil
 }
 
-func WithID(id string) func(context.Context, *Handler) error {
+func WithID(id *string) func(context.Context, *Handler) error {
 	return func(ctx context.Context, h *Handler) error {
-		if _, err := uuid.Parse(id); err != nil {
+		if id == nil {
+			return nil
+		}
+		if _, err := uuid.Parse(*id); err != nil {
 			return err
 		}
-		h.ID = &id
+		h.ID = id
+		return nil
+	}
+}
+
+func WithCreatedBy(id *string) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if id == nil {
+			return nil
+		}
+		if _, err := uuid.Parse(*id); err != nil {
+			return err
+		}
+		h.CreatedBy = id
+		return nil
+	}
+}
+
+func WithName(name *string) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if name == nil {
+			return nil
+		}
+		const leastNameLen = 3
+		if len(*name) < leastNameLen {
+			return fmt.Errorf("invalid name")
+		}
+		h.Name = name
+		return nil
+	}
+}
+
+func WithLogo(logo *string) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if logo == nil {
+			return nil
+		}
+		const leastLogoLen = 5
+		if len(*logo) < leastLogoLen {
+			return fmt.Errorf("invalid logo")
+		}
+		h.Logo = logo
+		return nil
+	}
+}
+
+func WithDescription(description *string) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.Description = description
+		return nil
+	}
+}
+
+func WithSignupMethods(methods []basetypes.SignMethod) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		for _, method := range methods {
+			switch method {
+			case basetypes.SignMethod_Mobile:
+			case basetypes.SignMethod_Email:
+			default:
+				return fmt.Errorf("invalid signup method")
+			}
+		}
+		h.SignupMethods = methods
+		return nil
+	}
+}
+
+func WithExtSigninMethods(methods []basetypes.SignMethod) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		for _, method := range methods {
+			switch method {
+			case basetypes.SignMethod_Twitter:
+				fallthrough //nolint
+			case basetypes.SignMethod_Wechat:
+				fallthrough //nolint
+			default:
+				return fmt.Errorf("invalid ext signin method")
+			}
+		}
+		h.ExtSigninMethods = methods
+		return nil
+	}
+}
+
+func WithRecaptchaMethod(method *basetypes.RecaptchaMethod) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if method == nil {
+			return nil
+		}
+		switch *method {
+		case basetypes.RecaptchaMethod_GoogleRecaptchaV3:
+		default:
+			return fmt.Errorf("invalid recaptcha method")
+		}
+		h.RecaptchaMethod = method
+		return nil
+	}
+}
+
+func WithKycEnable(enable *bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.KycEnable = enable
+		return nil
+	}
+}
+
+func WithSigninVerifyEnable(enable *bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.SigninVerifyEnable = enable
+		return nil
+	}
+}
+
+func WithInvitationCodeMust(must *bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.InvitationCodeMust = must
+		return nil
+	}
+}
+
+func WithCreateInvitationCodeWhen(when *basetypes.CreateInvitationCodeWhen) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		if when == nil {
+			return nil
+		}
+		switch *when {
+		case basetypes.CreateInvitationCodeWhen_Registration:
+		case basetypes.CreateInvitationCodeWhen_SetToKol:
+		case basetypes.CreateInvitationCodeWhen_HasPaidOrder:
+		default:
+			return fmt.Errorf("invalid create invitation code when")
+		}
+		h.CreateInvitationCodeWhen = when
+		return nil
+	}
+}
+
+func WithMaxTypedCouponsPerOrder(max *uint32) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.MaxTypedCouponsPerOrder = max
+		return nil
+	}
+}
+
+func WithMaintaining(maintaining *bool) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.Maintaining = maintaining
+		return nil
+	}
+}
+
+func WithCommitButtonTargets(targets []string) func(context.Context, *Handler) error {
+	return func(ctx context.Context, h *Handler) error {
+		h.CommitButtonTargets = targets
 		return nil
 	}
 }
