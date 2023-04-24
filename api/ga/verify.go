1ab722ea4f4d2a1d1c8c5be4de55ae0684d30d50
Zhao KK <kikakkz@hotmail.com>
2023-04-24 05:21:35 +0000

    Add apis for ga

---
+++ api/ga/verify.go
@@ -0,0 +1,47 @@
+package ga
+
+import (
+	"context"
+
+	ga1 "github.com/NpoolPlatform/appuser-gateway/pkg/ga"
+	"github.com/NpoolPlatform/go-service-framework/pkg/logger"
+	npool "github.com/NpoolPlatform/message/npool/appuser/gw/v1/ga"
+
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) VerifyGoogleAuth(
+	ctx context.Context, in *npool.VerifyGoogleAuthRequest,
+) (
+	*npool.VerifyGoogleAuthResponse, error,
+) {
+	handler, err := ga1.NewHandler(
+		ctx,
+		ga1.WithAppID(in.GetAppID()),
+		ga1.WithUserID(in.GetUserID()),
+		ga1.WithCode(in.GetCode()),
+	)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"VerifyGoogleAuth",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.VerifyGoogleAuthResponse{}, status.Error(codes.InvalidArgument, err.Error())
+	}
+
+	info, err := handler.VerifyGoogleAuth(ctx)
+	if err != nil {
+		logger.Sugar().Errorw(
+			"VerifyGoogleAuth",
+			"In", in,
+			"Error", err,
+		)
+		return &npool.VerifyGoogleAuthResponse{}, status.Error(codes.Internal, err.Error())
+	}
+
+	return &npool.VerifyGoogleAuthResponse{
+		Info: info,
+	}, nil
+}
