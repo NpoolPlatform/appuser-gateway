c5530c79ea5ae9a28bd7ce704e9d86a869a4e623
Zhao KK <kikakkz@hotmail.com>
2023-04-26 04:57:16 +0000

    Add uuid const for conds

--- pkg/authing/auth/create.go
+++ pkg/authing/auth/create.go
@@ -5,19 +5,21 @@ import (
 	"fmt"
 
 	authmwcli "github.com/NpoolPlatform/appuser-middleware/pkg/client/authing/auth"
+	uuid1 "github.com/NpoolPlatform/go-service-framework/pkg/const/uuid"
 	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	authmwpb "github.com/NpoolPlatform/message/npool/appuser/mw/v1/authing/auth"
 	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
 )
 
 func (h *Handler) CreateAuth(ctx context.Context) (*authmwpb.Auth, error) {
+	defaultUUIDCond := &basetypes.StringVal{Op: cruder.EQ, Value: uuid1.InvalidUUIDStr}
+
 	conds := &authmwpb.Conds{
 		AppID:    &basetypes.StringVal{Op: cruder.EQ, Value: h.AppID},
+		UserID:   defaultUUIDCond,
+		RoleID:   defaultUUIDCond,
 		Resource: &basetypes.StringVal{Op: cruder.EQ, Value: h.Resource},
 		Method:   &basetypes.StringVal{Op: cruder.EQ, Value: h.Method},
-	}
-	if h.UserID != nil {
-		conds.UserID = &basetypes.StringVal{Op: cruder.EQ, Value: *h.UserID}
 	}
 	exist, err := authmwcli.ExistAuthConds(ctx, conds)
 	if err != nil {
@@ -29,23 +31,28 @@ func (h *Handler) CreateAuth(ctx context.Context) (*authmwpb.Auth, error) {
 
 	if h.RoleID != nil {
 		conds.RoleID = &basetypes.StringVal{Op: cruder.EQ, Value: *h.RoleID}
-	}
-	h.UserID = nil
-	exist, err = authmwcli.ExistAuthConds(ctx, conds)
-	if err != nil {
-		return nil, err
-	}
-	if exist {
-		return nil, fmt.Errorf("auth already exist")
+		conds.UserID = defaultUUIDCond
+
+		exist, err = authmwcli.ExistAuthConds(ctx, conds)
+		if err != nil {
+			return nil, err
+		}
+		if exist {
+			return nil, fmt.Errorf("auth already exist")
+		}
 	}
 
-	h.RoleID = nil
-	exist, err = authmwcli.ExistAuthConds(ctx, conds)
-	if err != nil {
-		return nil, err
-	}
-	if exist {
-		return nil, fmt.Errorf("auth already exist")
+	if h.UserID != nil {
+		conds.UserID = &basetypes.StringVal{Op: cruder.EQ, Value: *h.UserID}
+		conds.RoleID = nil
+
+		exist, err = authmwcli.ExistAuthConds(ctx, conds)
+		if err != nil {
+			return nil, err
+		}
+		if exist {
+			return nil, fmt.Errorf("auth already exist")
+		}
 	}
 
 	return authmwcli.CreateAuth(ctx, &authmwpb.AuthReq{
