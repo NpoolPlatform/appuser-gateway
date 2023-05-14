4453e7b03cab7d5e12e4f3f306ad877d22b9f145
Zhao KK <kikakkz@hotmail.com>
2023-05-14 10:24:46 +0000

    Add default role for user

--- pkg/user/create.go
+++ pkg/user/create.go
@@ -2,9 +2,14 @@ package user
 
 import (
 	"context"
+	"fmt"
 
+	rolemwcli "github.com/NpoolPlatform/appuser-middleware/pkg/client/role"
 	usermwcli "github.com/NpoolPlatform/appuser-middleware/pkg/client/user"
+	"github.com/NpoolPlatform/libent-cruder/pkg/cruder"
+	rolemwpb "github.com/NpoolPlatform/message/npool/appuser/mw/v1/role"
 	usermwpb "github.com/NpoolPlatform/message/npool/appuser/mw/v1/user"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
 
 	"github.com/google/uuid"
 )
@@ -15,6 +20,17 @@ func (h *Handler) CreateUser(ctx context.Context) (*usermwpb.User, error) {
 		h.UserID = &id
 	}
 
+	role, err := rolemwcli.GetRoleOnly(ctx, &rolemwpb.Conds{
+		AppID:   &basetypes.StringVal{Op: cruder.EQ, Value: h.AppID},
+		Default: &basetypes.BoolVal{Op: cruder.EQ, Value: true},
+	})
+	if err != nil {
+		return nil, err
+	}
+	if role == nil {
+		return nil, fmt.Errorf("invalid default role")
+	}
+
 	return usermwcli.CreateUser(
 		ctx,
 		&usermwpb.UserReq{
@@ -23,6 +39,7 @@ func (h *Handler) CreateUser(ctx context.Context) (*usermwpb.User, error) {
 			EmailAddress: h.EmailAddress,
 			PhoneNO:      h.PhoneNO,
 			PasswordHash: h.PasswordHash,
+			RoleIDs:      []string{role.ID},
 		},
 	)
 }
