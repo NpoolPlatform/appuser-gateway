2e1dd0385cc091fe0420589f746246ec0c33185c
jakys <jakysing@163.com>
2023-11-13 06:20:12 +0000

    Fix http://47.116.78.131:10083/issues/2360

--- pkg/role/update.go
+++ pkg/role/update.go
@@ -2,14 +2,27 @@ package role
 
 import (
 	"context"
+	"fmt"
 
 	rolemwcli "github.com/NpoolPlatform/appuser-middleware/pkg/client/role"
+	cruder "github.com/NpoolPlatform/libent-cruder/pkg/cruder"
 	rolemwpb "github.com/NpoolPlatform/message/npool/appuser/mw/v1/role"
+	basetypes "github.com/NpoolPlatform/message/npool/basetypes/v1"
 )
 
 func (h *Handler) UpdateRole(ctx context.Context) (*rolemwpb.Role, error) {
-	if err := h.ExistRole(ctx); err != nil {
+	info, err := rolemwcli.GetRoleOnly(ctx, &rolemwpb.Conds{
+		ID:    &basetypes.Uint32Val{Op: cruder.EQ, Value: *h.ID},
+		EntID: &basetypes.StringVal{Op: cruder.EQ, Value: *h.EntID},
+	})
+	if err != nil {
 		return nil, err
+	}
+	if info == nil {
+		return nil, fmt.Errorf("invalid role")
+	}
+	if info.AppID != *h.AppID {
+		return nil, fmt.Errorf("permission denied")
 	}
 	return rolemwcli.UpdateRole(ctx, &rolemwpb.RoleReq{
 		ID:          h.ID,
